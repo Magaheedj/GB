a5ae3fded972b1a05e09deef2045a66227f1eaf9
Egor <edzumaev91@gmail.com>
2024-01-11 01:58:44 +0300

    Создал метод который проверякт условия и создает строки

--- gb_work/Program.cs
+++ gb_work/Program.cs
@@ -15,6 +15,33 @@ class Program
         string  stroka = Console.ReadLine()!;
         string[] array = stroka.Split(' ');
 
-   
+        
+    }
+
+    static string[] FilterArray(string[] array)
+    {
+        // Подсчет количества строк, удовлетворяющих условию
+        int count = 0;
+        foreach (string element in array)
+        {
+            if (element.Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        // Создание нового массива с подходящими строкам
+        string[] newArray = new string[count];
+        int index = 0;
+        foreach (string element in array)
+        {
+            if (element.Length <= 3)
+            {
+                newArray[index] = element;
+                index++;
+            }
+        }
+
+        return newArray;
     }
 }
